d4ddecfc892b9f464c37e2d9e0ca1113a0ca74ee
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-09-04 22:14:42 +0300

    button-callback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div className="Feedback">
+        <h2>Please leave feedback</h2>
+        <button type="button" onClick={() => console.log('Good')}>
+          Good
+        </button>
+        <button type="button" onClick={() => console.log('Neutral')}>
+          Neutral
+        </button>
+        <button type="button" onClick={() => console.log('Bad')}>
+          Bad
+        </button>
+        <h2 className="Stats" onClick={() => console.log('Statistics')}>
+          Statistics
+        </h2>
+        <p>Good:</p>
+        <p>Neutral:</p>
+        <p>Bad:</p>
+        <p>Positive fedback:</p>
+      </div>
+    );
+  }
+}
+
+export default App;
